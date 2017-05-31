278a42d645502d8de528b04abf0169c58fa22613
Jesse Gwinn <gwinn092@gmail.com>
2017-05-31 14:53:48 -0600

    25

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+        <div className= "ui unstackable items">
+          Hello, friend! I am a basic react component.
+        </div>
+    );
+  }
+}
 
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
